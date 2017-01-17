89f0b3412f1f845bcddaf17c4e80f1dd6c877536
Alfredo Bernabe <luis.alfredo.bernabe@gmail.com>
2017-01-16 19:09:23 -0500

    imicial

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Nombre2 } from './components/nombre';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React alfredo gggg</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  const nombre='alfredo'
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Nombre2 n='ggggggggggggsresfsdfsdf' l='antes'/>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nombre.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Nombre2({n='aaaaaa',l}) {
+  console.log(n)
+  return (
+    <h2>{l} Welcome to React {n}</h2>
+  )
+}
+
+ // Nombre2;
